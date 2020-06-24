402bee0bf5380b9c4ac98feeb080ed91faa662e1
Levin Weinstein <weinsteinlevin@gmail.com>
2020-06-24 14:23:14 -0700

    add auth reouter to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const todoRouter = require('./todoRouter');
+const authenticationRouter = require('./authenticationRouter')
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/todos", todoRouter)
-
+app.use("/auth", authenticationRouter)
 
 app.get("/", (req, res, next) => {
     res.send("Hello!");
